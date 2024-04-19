54c736b28e5255b8128c3bd7e916ce2e01030534
TurtleOld <dev@pavlovteam.ru>
2024-04-19 18:29:23 +0300

    pass 6nd stage

--- app/main.py
+++ app/main.py
@@ -1,35 +1,40 @@
 # Uncomment this to pass the first stage
 import socket
+from _thread import start_new_thread
+
+
+def client_handler(connection):
+    while True:
+        data = connection.recv(4096)
+        
+        if not data:
+            break
+        
+        data = data.decode('utf-8')
+        path = data.split()[1]
+        if path == '/':
+            response = 'HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n'
+            connection.sendall(response.encode())
+        elif path.startswith("/echo"):
+            echo = path[6:]
+            response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}'
+            connection.sendall(response.encode())
+        elif path.startswith('/user-agent'):
+            user_agent = data.split()[6] if len(data.split()) > 6 else ''
+            response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}'
+            connection.sendall(response.encode())
+        else:
+            response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+            connection.sendall(response.encode())
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection, address = server_socket.accept()
-    with connection:
-        print("Connected by", address)
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                break
-            path = data.decode().split()[1]
-            user_agent = data.decode().split()[6]
-            if path == "/":
-                connection.send(b'HTTP/1.1 200 OK\r\n\r\n')
-            elif path.startswith("/echo"):
-                echo = path[6:]
-                response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\n{echo}\r\n\r\n'
-                connection.send(response.encode())
-            elif path.startswith('/user-agent'):
-                response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\n{user_agent}\r\n\r\n'
-                connection.send(response.encode())
-            else:
-                connection.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
-            
+    
+    while True:
+        connection, _ = server_socket.accept()
+        start_new_thread(client_handler, (connection,))
+        
 
 if __name__ == "__main__":
     main()
